74e5e0c8ed7ba31f0e01ed5bc7dc6e22343f07b4
LukeGu <luke.gu.work@gmail.com>
2023-09-05 21:00:16 +1000

    create user apis
    signup
    login
    getUserByEmail
    updateUserByEmail
    deleteUserByEmail

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,19 +1,174 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { pool } from '../services/db.js';
+
+const saltRounds = 10;
+const secret = 'smart assistance secret';
 
 export const createUser = async (request, response) => {
   const { firstname, lastname, email, password } = request.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  const client = await pool.connect();
+  try {
+    const user = await client.query('SELECT * FROM users WHERE email = $1', [
+      email,
+    ]);
+    if (!user.rows[0]) {
+      const result = await client.query(
+        'INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
+        [firstname, lastname, email, hashPassword]
+      );
+      console.log('CreateUser', result.rows[0]);
+      response.status(201).json({
+        success: true,
+        message: 'Added a new user',
+        newUser: result.rows[0],
+      });
+    } else {
+      response.status(409).json({
+        success: false,
+        message: 'User already existed',
+      });
+    }
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  } finally {
+    client.release();
+  }
+};
+
+// GetUserByEmail
+export const getUserByEmail = async (request, response) => {
+  const email = request.params.email;
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT * FROM users WHERE email = $1', [
+      email,
+    ]);
+    console.log('getUser', result.rows[0]);
+    if (!result.rows[0]) {
+      response.status(400).json({
+        success: false,
+        message: 'Not found the user',
+      });
+    } else {
+      response.status(200).json({
+        success: true,
+        message: 'Found the user',
+        user: result.rows[0],
+      });
+    }
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  } finally {
+    client.release();
+  }
+};
+
+// login
+export const login = async (request, response) => {
+  const { email, password } = request.body;
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM users WHERE email = $1', [
+      email,
+    ]);
+    const isCorrect = bcrypt.compareSync(password, result.rows[0].password); // true
+    console.log('Login successfully', email);
+    if (isCorrect) {
+      var token = jwt.sign(
+        {
+          firstname: result.rows[0].firstname,
+          lastname: result.rows[0].lastname,
+          email: result.rows[0].email,
+        },
+        secret,
+        { expiresIn: '1h' }
+      );
+      response.status(200).json({
+        success: true,
+        message: 'Login successfully',
+        token: token,
+      });
+    } else {
+      response.status(400).json({
+        success: false,
+        message: 'Your email or password is incorrect',
+      });
+    }
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  } finally {
+    client.release();
+  }
+};
+
+// forgot password
+
+// edit profile
+export const updateUserByEmail = async (request, response) => {
+  const email = request.params.email;
+  const { firstname, lastname } = request.body;
   const client = await pool.connect();
   try {
     const result = await client.query(
-      'INSERT INTO users (firstname,lastname, email,password) VALUES ($1, $2, $3, $4) RETURNING *',
-      [firstname, lastname, email, password]
+      'UPDATE users SET firstname = $1, lastname = $2 WHERE email = $3',
+      [firstname, lastname, email]
     );
-    console.log('CreateUser', result.rows[0]);
-    response.status(201).json({
-      success: true,
-      message: 'Added a new user',
-      newUser: result.rows[0],
+    console.log('User profile updated', email, result);
+    if (!result.rows[0]) {
+      response.status(400).json({
+        success: false,
+        message: 'Not found the user',
+      });
+    } else {
+      response.status(200).json({
+        success: true,
+        message: 'User profile updated successfully',
+        updatedUser: result.rows[0],
+      });
+    }
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      message: error.message,
     });
+  } finally {
+    client.release();
+  }
+};
+
+// Delete user
+export const deleteUserByEmail = async (request, response) => {
+  const email = request.params.email;
+  const client = await pool.connect();
+  try {
+    const result = await client.query('DELETE FROM users WHERE email = $1', [
+      email,
+    ]);
+    console.log('Delete a user', email, result.rows[0]);
+    if (!result.rows[0]) {
+      response.status(400).json({
+        success: false,
+        message: 'Not found the user',
+      });
+    } else {
+      response.status(200).json({
+        success: true,
+        message: 'Delete a user successfully',
+      });
+    }
   } catch (error) {
     response.status(500).json({
       success: false,
--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
 // import { pool, query } from './db.js';
-import { createUser } from '../controllers/user.controller.js';
+import {
+  createUser,
+  getUserByEmail,
+  updateUserByEmail,
+  deleteUserByEmail,
+  login,
+} from '../controllers/user.controller.js';
 
 const userRouter = Router();
 userRouter.post('/signup', createUser);
+userRouter.post('/login', login);
+userRouter.get('/getUserByEmail/:email', getUserByEmail);
+userRouter.put('/updateUserByEmail/:email', updateUserByEmail);
+userRouter.delete('/deleteUserByEmail/:email', deleteUserByEmail);
 
 export default userRouter;
